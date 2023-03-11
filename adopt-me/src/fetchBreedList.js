3d7e1b890703ae6cdf4d9b6a92245613f7a16432
Rizvan <rizvan.chalilovas@proton.me>
2023-03-11 11:40:49 +0200

    fix unconditional breed list fetching bug

--- adopt-me/src/fetchBreedList.js
+++ adopt-me/src/fetchBreedList.js
@@ -1,10 +1,11 @@
 const fetchBreedList = async ({ queryKey }) => {
   const animal = queryKey[1];
+
+  if (!animal) return [];
+
   const apiRes = await fetch(
     `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
-
-  if (!animal) return [];
 
   if (!apiRes.ok) {
     throw new Error(`breeds/${animal} fetch not 200`);
